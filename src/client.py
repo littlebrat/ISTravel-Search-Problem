9810e48782f354b2a081b3087e7f09f171fa23ff
Nuno Mendes <mrgugas1993@gmail.com>
2015-10-10 16:37:45 +0100

    Problem reading done, added test files

--- src/client.py
+++ src/client.py
@@ -1,7 +1,7 @@
 import math
 
 
-class Client:
+class Problem:
 
     def __init__(self, idn, start, goal, available, criteria):
         self.client_id = idn
@@ -19,16 +19,33 @@ class Client:
         self.unwanted_ride = ride
 
     def set_max_ride_cost(self, number):
-        self.maximum_ride_cost = number
+        self.maximum_ride_cost = int(number)
 
     def set_max_ride_dur(self, number):
-        self.maximum_ride_duration = number
+        self.maximum_ride_duration = int(number)
 
     def set_max_plan_cost(self, number):
-        self.maximum_plan_cost = number
+        self.maximum_plan_cost = int(number)
 
     def set_max_plan_dur(self, number):
-        self.maximum_plan_duration = number
+        self.maximum_plan_duration = int(number)
+
+    def make_restriction(self, key, value):
+        restriction_list = {'A1': self.set_unwanted_ride, 'A2': self.set_max_ride_dur,
+                            'A3': self.set_max_ride_cost, 'B1': self.set_max_plan_dur,
+                            'B2': self.set_max_plan_cost}
+        restriction_list[key](value)
+
+    def __str__(self):
+        res = "{id: " + str(self.client_id) + ', start: ' + str(self.start_node) + ', goal: ' \
+              + str(self.goal_node) + ', available at: ' + str(self.available_time) \
+              + ', optimization criteria: ' + self.optimization_criteria \
+              + ', unwanted ride: ' + str(self.unwanted_ride) \
+              + ', max ride cost: ' + str(self.maximum_ride_cost) + ', max ride duration: ' \
+              + str(self.maximum_ride_duration) + ', max plan cost: ' + str(self.maximum_plan_cost) \
+              + ', max plan duration: ' + str(self.maximum_plan_duration)
+        return res
+
 
 class Pawns:
 
@@ -45,12 +62,26 @@ class Pawns:
                     # verifying the number of clients
                     clients_req = int(words[0])
                 elif len(words) is 6 and words[5] is '0':
-                    aux = Client(words[0], int(words[1]), int(words[2]), int(words[3]), words[4])
+                    aux = Problem(words[0], int(words[1]), int(words[2]), int(words[3]), words[4])
                     self.clients.append(aux)
                 elif (len(words) is 8 or 10) and (words[5] is '1' or '2'):
-                    aux = Client(words[0], int(words[1]), int(words[2]), int(words[3]), words[4])
-
+                    aux = Problem(words[0], int(words[1]), int(words[2]), int(words[3]), words[4])
+                    aux.make_restriction(words[6], words[7])
+                    if words[5] is '2':
+                        aux.make_restriction(words[8], words[9])
                     self.clients.append(aux)
                 else:
                     # return an exception if we don't find the expected number of words in a line.
                     return Exception('Wrong file format.')
+
+    def __str__(self):
+        res = ''
+        for c in self.clients:
+            res += str(c) + '\n'
+        return res
+
+# testing
+print("\n>> test 1\n")
+clients = Pawns()
+clients.from_file('client_files/client_test.cli')
+print(clients)
